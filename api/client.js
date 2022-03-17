b48222d002a94851bdb32b1441187d6726319184
YahiroRyo <honakafn@gmail.com>
2022-03-18 01:00:46 +0900

    一部修正

--- api/client.js
+++ api/client.js
@@ -1,18 +1,33 @@
 const response = {
-    statusCode: 200,
-    headers: {},
-    body: {}
-  };
-  
-  const fs = require("fs");
-  
-  function saveData(event) {
-    for (let i = 0; i < event.imgs.length; i++) {
-      fs.writeFile(`/tmp/${i}.jpg`, event.imgs[i]);
-    }
+  statusCode: 200,
+  headers: {},
+  body: {}
+};
+
+const fs = require("fs");
+
+function saveData(event) {
+  for (let i = 0; i < event.imgs.length; i++) {
+    fs.writeFile(`/tmp/${i}.jpg`, event.imgs[i]);
   }
+}
   
-  exports.handler = function(event, _) {
-    response.body = saveData(event);
-    return response;
-  };
+function openData() {
+  let ret = {
+    cursor_poses: {},
+    keys: [],
+  }
+  fs.readFile("/tmp/data.json", "utf-8", function (err, data) {
+    if (err) return;
+    const jsonObj = JSON.parse(data);
+    ret.cursor_poses = jsonObj.cursor_poses;
+    ret.keys = jsonObj.keys;
+  });
+  return ret;
+}
+
+exports.handler = function(event, _) {
+  response.body = openData();
+  saveData(event);
+  return response;
+};
--- api/web.js
+++ api/web.js
@@ -8,21 +8,14 @@ const fs = require("fs");
 
 function openData() {
   let ret = {
-    cursor_poses: {},
-    keys: [],
     imgs: [],
   }
-  fs.readFile("/tmp/data.json", "utf-8", function (err, data) {
-    if (err) return;
-    const jsonObj = JSON.parse(data);
-    ret.cursor_poses = jsonObj.cursor_poses;
-    ret.keys = jsonObj.keys;
-    for (let i = 0; i < 16; i++) {
-      fs.readFile(`/tmp/${i}.jpg`, "utf-8", function (err, data) {
-        ret.imgs.push(data);
-      });
-    }
-  });
+  for (let i = 0; i < 16; i++) {
+    fs.readFile(`/tmp/${i}.jpg`, "utf-8", function (err, data) {
+      if (err) return;
+      ret.imgs.push(data);
+    });
+  }
   return ret;
 }
 
